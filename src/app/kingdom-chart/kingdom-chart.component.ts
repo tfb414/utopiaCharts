c6dd9fa36cf934af6c1c63ea206f3d4c318bc16f
Tim Brady <txb3flf@c02w3akmhtd6mbp.attlocal.net>
2018-05-28 15:46:23 -0400

    end point working

--- src/app/kingdom-chart/kingdom-chart.component.ts
+++ src/app/kingdom-chart/kingdom-chart.component.ts
@@ -9,55 +9,56 @@ import { Chart } from 'chart.js'
 })
 export class KingdomChartComponent implements OnInit {
 
-  chart = [];
+  chart: any = [];
   
   constructor(private _weather: WeatherService) {}
 
   ngOnInit() {
     this._weather.dailyForecast()
       .subscribe(res => {
-        
-        let temp_max = res['list'].map(res => res.main.temp_max)
-        let temp_min = res['list'].map(res => res.main.temp_min)
-        let alldates = res['list'].map(res => res.dt)
 
-        let weatherDates = []
-        alldates.forEach((res) => {
-          let jsdate = new Date(res * 1000)
-          weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric'}))
-        })
+        console.log(res);
+        // let temp_max = res['list'].map(res => res.main.temp_max)
+        // let temp_min = res['list'].map(res => res.main.temp_min)
+        // let alldates = res['list'].map(res => res.dt)
 
-        this.chart = new Chart('canvas', {
-          type: 'line',
-          data: {
-            labels: weatherDates,
-            datasets: [
-              {
-                data: temp_max,
-                borderColor: '#3cba9f',
-                fill: false
-              },
-              {
-                data: temp_min,
-                borderColor: '#ffcc00',
-                fill: false
-              },
-            ]
-          },
-          options: {
-            legend: {
-              display: false
-            },
-            scales: {
-              xAxes: [{
-                display: true
-              }],
-              yAxes: [{
-                display: true
-              }]
-            }
-          }
-        })
+        // let weatherDates = []
+        // alldates.forEach((res) => {
+        //   let jsdate = new Date(res * 1000)
+        //   weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric'}))
+        // })
+
+      //   this.chart = new Chart('canvas', {
+      //     type: 'line',
+      //     data: {
+      //       labels: weatherDates,
+      //       datasets: [
+      //         {
+      //           data: temp_max,
+      //           borderColor: '#3cba9f',
+      //           fill: false
+      //         },
+      //         {
+      //           data: temp_min,
+      //           borderColor: '#ffcc00',
+      //           fill: false
+      //         },
+      //       ]
+      //     },
+      //     options: {
+      //       legend: {
+      //         display: false
+      //       },
+      //       scales: {
+      //         xAxes: [{
+      //           display: true
+      //         }],
+      //         yAxes: [{
+      //           display: true
+      //         }]
+      //       }
+      //     }
+      //   })
 
       })
   }
